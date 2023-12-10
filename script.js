31c16be3c93142762caf4d48c2ebbb3854ab0d4f
MarkVeber <100853275+MarkVebeer@users.noreply.github.com>
2023-12-10 11:09:00 +0100

    update

---
+++ script.js
@@ -0,0 +1,51 @@
+
+
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Szugorodó navbar
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    navbarShrink();
+
+    // Ha görgetnek, akkor is zsugorodik a navbar
+    document.addEventListener('scroll', navbarShrink);
+
+    // Aktiválja a Bootstrap scrollspy-t
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // responsivvá teszi a navbar-t
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-linking')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+
+
+
