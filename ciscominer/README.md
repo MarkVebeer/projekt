149d20f734f841b1ee8c548a1609edad4494523f
MarkVeber <100853275+MarkVebeer@users.noreply.github.com>
2024-05-21 11:21:08 +0200

    erikh

---
+++ ciscominer/README.md
@@ -0,0 +1,4 @@
+# CiscoMiner
+
+Ez itt a cisco klikker, itt mi nyomogatjuk a cisco eszközöket. yo?
+
--- script.js
+++ script.js
@@ -17,7 +17,6 @@ sr.reveal('.hero-text',{delay: 200, origin:'top'});
 sr.reveal('.hero-img',{delay: 450, origin:'top'});
 sr.reveal('.icons',{delay: 500, origin:'left'});
 sr.reveal('.scroll-down',{delay: 500, origin:'right'});
-sr.reveal('.norbi-text',{delay: 500, origin:'top'});
 
 const sections = document.querySelectorAll('section[id]');
 
